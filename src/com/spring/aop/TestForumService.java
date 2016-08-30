c3b90580bb1dd0dfc700cd2b5e45569099c9afa9
dk05408 <dongkun.sh@superjia.com>
2016-08-30 17:29:05 +0800

    spring proxy cglib

--- src/com/spring/aop/TestForumService.java
+++ src/com/spring/aop/TestForumService.java
@@ -17,7 +17,7 @@ public class TestForumService {
 
         //********************************************
 
-   /*     //①希望被代理的目标业务类
+        //①希望被代理的目标业务类
         ForumService target = new ForumServiceImpl();
 
         //②将目标业务类和横切代码编织到一起
@@ -32,14 +32,36 @@ public class TestForumService {
         //④调用代理实例
         proxy.removeForum(10);
         System.out.println("");
-        proxy.removeTopic(1012);*/
+        proxy.removeTopic(1012);
+
+   /*     console out:
+        begin monitor....
+        模拟删除Forum记录:10
+        end monitor....
+        com.spring.aop.ForumServiceImpl.removeForum花费40毫秒。
+
+        begin monitor....
+        模拟删除Topic记录:1012
+        end monitor....
+        com.spring.aop.ForumServiceImpl.removeTopic花费21毫秒。*/
+
       //********************************************
 
         //cglib
-        CglibProxy proxy = new CglibProxy();
-        ForumServiceImpl forumService = //①
-        (ForumServiceImpl )proxy.getProxy(ForumServiceImpl.class);
-        forumService.removeForum(10);
-        forumService.removeTopic(1023);
+//        CglibProxy proxy = new CglibProxy();
+//        ForumServiceImpl forumService = //①
+//        (ForumServiceImpl )proxy.getProxy(ForumServiceImpl.class);
+//        forumService.removeForum(10);
+//        forumService.removeTopic(1023);
+
+   /*   console out:
+        begin monitor....
+        模拟删除Forum记录:10
+        end monitor....
+        com.spring.aop.ForumServiceImpl$$EnhancerByCGLIB$$f5731c05.removeForum花费40毫秒。
+        begin monitor....
+        模拟删除Topic记录:1023
+        end monitor....
+        com.spring.aop.ForumServiceImpl$$EnhancerByCGLIB$$f5731c05.removeTopic花费20毫秒。*/
     }
 }
