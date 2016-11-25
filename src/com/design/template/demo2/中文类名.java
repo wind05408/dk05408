540f28c1a15f48bb65906e95f8a49d9b788dda2f
dk05408 <dongkun.sh@superjia.com>
2016-11-25 11:19:54 +0800

    中文类与方法

---
+++ src/com/design/template/demo2/中文类名.java
@@ -0,0 +1,17 @@
+package com.design.template.demo2;
+
+/**
+ * 中文类名
+ *
+ * @author dk
+ * @date 2016/11/25
+ */
+public class 中文类名 {
+    public static void main(String[] args) {
+        中文类名 中文类名 = new 中文类名();
+        中文类名.中文方法名("中亠字条码测试");
+    }
+    public void  中文方法名(String 中文字符){
+        System.out.println(中文字符);
+    }
+}
