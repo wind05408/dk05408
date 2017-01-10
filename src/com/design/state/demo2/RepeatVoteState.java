629e76221d2e8c5c547ad70206f49aa92bfeee14
dk05408 <dongkun.sh@superjia.com>
2017-01-10 15:02:32 +0800

    state model

--- src/com/design/state/demo2/RepeatVoteState.java
+++ src/com/design/state/demo2/RepeatVoteState.java
@@ -6,7 +6,7 @@ package com.design.state.demo2;
 public class RepeatVoteState implements VoteState {
     @Override
     public void vote(String user, String voteItem, VoteManager voteManager) {
-//重复投票，暂时不做处理
+        //重复投票，暂时不做处理
         System.out.println("请不要重复投票");
     }
 }
